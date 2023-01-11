36f799cf47416d9af828adef97969292deedbc7e
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-01-11 21:37:48 +0100

    skeleton loader

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,17 +1,22 @@
 let about_template = 
 `
-    <div href="" class="h-96 bg-red-500 hover:scale-90 transition-transform duration-500 ease-out text-center space-y-5 col-span-1 mx-auto w-ninetyPercent sm:w-auto">
-    <div class="bg-brandWhite2 max-w-60 sm:max-w-none h-52 sm:h-72 sm:w-72">
-    </div>
-        <div>
-            <div class="font-biotifMedium text-lg text-brandBlue2">${data[i].name}</div>
-            <div class="text-sm text-brandBlack1 font-biotifBook">${data[i].role}</div>
+    <div href="" class="shadow-md h-96 bg-white hover:scale-90 transition-transform duration-500 ease-out text-center space-y-5 col-span-1 mx-auto w-ninetyPercent sm:w-auto">
+    <div class="bg-brandWhite2 skeleton-template max-w-60 sm:max-w-none h-52 sm:h-72 sm:w-72">
+    </div class="w-full">
+        <div class="w-full flex flex-col gap-3 items-center">
+            <div class="h-4 px-20 w-sixtyPercent bg-brandWhite2 skeleton-template py-2"></div>
+            <div class="h-4 px-10 w-fortyPercent bg-brandWhite2 skeleton-template py-2"></div>
         </div>
     </div>
 `
 
-function addTemplate(section, template){
-    $('#' + section).html(template)
+function addTemplate(section, template, repeat){
+    let templateList = ''
+    for (let i = 0; i < repeat; i++) {
+        templateList += template
+        
+    }
+    $('.' + section).html(templateList)
 }
 
-addTemplate('aboutEmployeesSection', about_template)
+addTemplate('show-about-template', about_template, 4)
